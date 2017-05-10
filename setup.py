b6f4b37dc718535f0dffef2f95bda0101ef9db69
Peter Bittner <peter@painless.software>
2017-05-11 00:11:46 +0200

    Add Python 3.5 and 3.6 to Tox setup
    see https://blog.codeship.com/improved-python-version-management-and-caching-on-codeship-basic/

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Software Development :: Build Tools',
 ]
 KEYWORDS = [
